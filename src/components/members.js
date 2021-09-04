b38d2146db85a2ee3c0dfd470136711156d28128
sarthak264 <sarthaksharm372@gmail.com>
2021-09-05 00:43:34 +0530

    made a list of members for about page

---
+++ src/components/members.js
@@ -0,0 +1,62 @@
+export default members = [
+    {
+        name:'Jioke Ugoorji',
+        id: 1,
+        image: './images/about us/team/mate1.png'
+    },
+    {
+        name:'Mahnaz Farzin',
+        id: 2,
+        image: './images/about us/team/mate2.png'
+    },
+    {
+        name:'Jeremías Romero',
+        id: 3,
+        image: './images/about us/team/mate3.png'
+    },
+    {
+        name:'Richardo Kann',
+        id: 4,
+        image: './images/about us/team/mate4.png'
+    },
+    {
+        name:'Henry Itondo',
+        id: 5,
+        image: './images/about us/team/mate5.png'
+    },
+    {
+        name:'Jaroslav Brabec',
+        id: 6,
+        image: './images/about us/team/mate6.png'
+    },
+    {
+        name:'Marleah Eagleston',
+        id: 7,
+        image: './images/about us/team/mate7.png'
+    },
+    {
+        name:'Svyatoslav Taushev',
+        id: 8,
+        image: './images/about us/team/mate8.png'
+    },
+    {
+        name:'Tokunaga Yae',
+        id: 9,
+        image: './images/about us/team/mate9.png'
+    },
+    {
+        name:'Kari Granleese',
+        id: 10,
+        image: './images/about us/team/mate10.png'
+    },
+    {
+        name:'Nahia Colunga',
+        id: 11,
+        image: './images/about us/team/mate11.png'
+    },
+    {
+        name:'Mgbankwo Orjee',
+        id: 12,
+        image: './images/about us/team/mate12.png'
+    },
+];
