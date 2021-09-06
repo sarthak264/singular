7cb7b69774211df07550e8dc33835bc7da74c957
sarthak264 <sarthaksharm372@gmail.com>
2021-09-06 16:38:37 +0530

    integrations list made

---
+++ src/components/Integrations.js
@@ -0,0 +1,110 @@
+export const Integration = [
+    {
+        name: 'react',
+        image: './images/integration/brand1.png'
+    },
+    {
+        name: 'google',
+        image: './images/integration/brand2.png'
+    },
+    {
+        name: 'snapchat',
+        image: './images/integration/brand3.png'
+    },
+    {
+        name: 'sketch',
+        image: './images/integration/brand4.png'
+    },
+    {
+        name: 'figma',
+        image: './images/integration/brand5.png'
+    },
+    {
+        name: 'invision',
+        image: './images/integration/brand6.png'
+    },
+    {
+        name: 'xd',
+        image: './images/integration/brand7.png'
+    },
+    {
+        name: 'spotify',
+        image: './images/integration/brand8.png'
+    },
+    {
+        name: 'zeplin',
+        image: './images/integration/brand9.png'
+    },
+    {
+        name: 'office',
+        image: './images/integration/brand10.png'
+    },
+    {
+        name: 'salesforce',
+        image: './images/integration/brand11.png'
+    },
+    {
+        name: 'etsy',
+        image: './images/integration/brand12.png'
+    },
+    {
+        name: 'dribbble',
+        image: './images/integration/brand13.png'
+    },
+    {
+        name: 'word',
+        image: './images/integration/brand14.png'
+    },
+    {
+        name: 'slack',
+        image: './images/integration/brand15.png'
+    },
+    {
+        name: 'telegram',
+        image: './images/integration/brand16.png'
+    },
+    {
+        name: 'line',
+        image: './images/integration/brand17.png'
+    },
+    {
+        name: 'drive',
+        image: './images/integration/brand19.png'
+    },
+    {
+        name: 'analytics',
+        image: './images/integration/brand21.png'
+    },
+    {
+        name: 'airbnb',
+        image: './images/integration/brand22.png'
+    },
+    {
+        name: 'excel',
+        image: './images/integration/brand23.png'
+    },
+    {
+        name: 'talk',
+        image: './images/integration/brand24.png'
+    },
+    {
+        name: 'kik',
+        image: './images/integration/brand25.png'
+    },
+    {
+        name: 'powerpoint',
+        image: './images/integration/brand26.png'
+    },
+    {
+        name: 'vs',
+        image: './images/integration/brand27.png'
+    },
+    {
+        name: 'evernote',
+        image: './images/integration/brand28.png'
+    },
+    {
+        name: 'onedrive',
+        image: './images/integration/brand29.png'
+    },
+]
